083413a76c4cac2cb9322af2dcb774b7d9df2823
ckdtjs505 <ckdtjs505@naver.com>
2024-03-18 21:10:48 +0900

    fix : 경로를 찾지 못하는 이슈 수정

--- js/feature/userProgressBibleReading/index.js
+++ js/feature/userProgressBibleReading/index.js
@@ -1,4 +1,7 @@
-import userProgressBibleReadingServices from "./userProgressBibleReadingServices.js";
+// import userProgressBibleReadingServices from "./userProgressBibleReadingServices.js";
+
+const GOOGLE_API = `https://script.google.com`;
+const GOOGLE_KEY = `AKfycbwHiTDeqtNUONNMdS5nekjTPponxGXkXMAM56So9S9LddNrGEk9kJY1QqlZelZefmUD`;
 
 export default class UserProgressBibleReading {
     constructor(userId){
@@ -6,7 +9,27 @@ export default class UserProgressBibleReading {
     }
 
     async loadUserProgress(){
-        return userProgressBibleReadingServices.getUserReadingProgress(this.userId);
-    }
 
+        // return userProgressBibleReadingServices.getUserReadingProgress(this.userId);
+        const queryParams = new URLSearchParams({
+            type: 'userProgress',
+            userName : this.userId
+        });
+        return fetch(
+            `${GOOGLE_API}/macros/s/${GOOGLE_KEY}/exec?${queryParams}`,
+            {
+                redirect: 'follow',
+                headers: {
+                    'Content-Type': 'text/plain;charset=utf-8',
+                },
+            }
+        )
+        .then((response) => {
+            const res =  response.json();
+            return res;
+        }).catch( (e) => {
+            console.error(`어드민 정보를 못가지고 옴`)
+        });
+        
+    }
 }
