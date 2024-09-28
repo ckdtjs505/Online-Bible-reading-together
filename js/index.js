4886ea657edf6c9765124a3f6f16a3dde94727f8
ckdtjs505 <ckdtjs505@naver.com>
2024-09-28 17:23:45 +0900

    feat: 함온성 우측 하단 위 화살표 생성

--- js/index.js
+++ js/index.js
@@ -8,3 +8,30 @@ document.addEventListener('DOMContentLoaded', () => {
     BibleTypeInit();
     calenderInit();
 });
+
+let lastScroll;
+document.addEventListener('scroll', ()=> {
+    console.log('scroll')
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    // 화면에 표시되는 높이 (viewport height)
+    const windowHeight = window.innerHeight;
+    
+    // 전체 페이지 높이
+    const documentHeight = document.documentElement.scrollHeight;
+
+    if(scrollTop > lastScroll){
+        // 다운 스크롤 
+        document.querySelector("#uparrow").style.display = 'none';
+    }else {
+        // 업 스크롤
+        document.querySelector("#uparrow").style.display = 'block';
+    }
+
+    if (scrollTop + windowHeight >= documentHeight) {
+        // 페이지의 마지막
+        document.querySelector("#uparrow").style.display = 'block';
+    }
+
+    lastScroll = scrollTop;
+})
