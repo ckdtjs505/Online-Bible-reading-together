a311c7a5ca18edbdcf7e829f9f6e603389d61958
소니아_오창선 <ckdtjs505@sooplive.com>
2025-01-09 12:12:12 +0900

    🐛 fix(daliyverse): 간헐적으로 다른 말씀이 노출되는 현상 수정

--- js/feature/dailyVerse/index.js
+++ js/feature/dailyVerse/index.js
@@ -4,35 +4,56 @@ import DailyVerseDisplay from "./DailyVerseDisplay.js";
 import DailyVerseService from "./DailyVerseServices.js";
 
 const dailyVerseInit = async () => {
-    const currentDay = new Date().getDate();
-    const currentYear = new Date().getFullYear(); 
-    const currentMonth = new Date().getMonth() + 1; 
+  const currentDay = new Date().getDate();
+  const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().getMonth() + 1;
 
-    const readingPlan = getReadingPlanForDate(`${currentYear}-${currentMonth}-${currentDay}`);
-    setVerse(readingPlan)
-    AppState.getInstance().readingPlan = readingPlan;
-}
+  const readingPlan = getReadingPlanForDate(
+    `${currentYear}-${currentMonth}-${currentDay}`,
+  );
+  setVerse(readingPlan);
+  AppState.getInstance().readingPlan = readingPlan;
+};
 
+let currentPromise = null;
 const setVerse = async (readingPlan, type) => {
-    const promises = readingPlan.map( async ({start, end, lang, book})  =>  {
-        let result;
+  const promises = readingPlan.map(async ({ start, end, lang, book }) => {
+    let result;
 
-        if( AppState.getInstance().bibleType == "KIV" ){
-            result = await DailyVerseService.getDailyVerseInKIV({ start, end, lang, book});
-        }else {
-            result = await DailyVerseService.getDailyVerseInKorean({ start, end, lang, book});
-        }
+    if (AppState.getInstance().bibleType == "KIV") {
+      result = await DailyVerseService.getDailyVerseInKIV({
+        start,
+        end,
+        lang,
+        book,
+      });
+    } else {
+      result = await DailyVerseService.getDailyVerseInKorean({
+        start,
+        end,
+        lang,
+        book,
+      });
+    }
 
-        // 비동기 작업의 결과와 해당 결과의 'book' 키를 객체로 반환
-        return { book, result };
-    })
-    
-    const verseDisplay = new DailyVerseDisplay('content')
+    // 비동기 작업의 결과와 해당 결과의 'book' 키를 객체로 반환
+    return { book, result };
+  });
+
+  currentPromise = promises;
+  try {
+    const verseDisplay = new DailyVerseDisplay("content");
     verseDisplay.renderLoading();
     const versesModels = await Promise.all(promises);
-    verseDisplay.setVerse(versesModels)
-    verseDisplay.render();
-    verseDisplay.renderLocalStorage();
-}
 
-export {dailyVerseInit, setVerse};
+    if (currentPromise === promises) {
+      verseDisplay.setVerse(versesModels);
+      verseDisplay.render();
+      verseDisplay.renderLocalStorage();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { dailyVerseInit, setVerse };
